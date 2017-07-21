e88b9725802b8fba5d83fdc77c00ee1d107f3c6d
VELA TIRADO DAVID <david.vela.tirado@gmail.com>
2017-07-21 23:14:31 +0000

    7. master-detail last touch - object Page!

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,18 +1,55 @@
 sap.ui.define([
 	"jquery.sap.global",
-	"sap/ui/core/mvc/Controller"
-], function(jQuery, Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(jQuery, Controller, JSONModel) {
 	"use strict";
 	return Controller.extend("mFFTwoMyFirstFioriTwoPZ.controller.Detail", {
-		onInit: function() {},
+		onInit: function() {
+			this.getRouter().attachRouteMatched(this._handleRouteChanged.bind(this));
+		},
+		_itemDetail: null,
+		_handleRouteChanged: function(oEvent) {
+			var oView = this.oView,
+				oModel = this.oView.getModel(),
+				oData = oModel.getData(),
+				sProductId = oEvent.getParameter("arguments")["detail-item"];
+				
+			this._itemDetail = sProductId;
+	
+			/*Uncaught (in promise) TypeError: Cannot read property 'filter' of undefined	=S */
+			var	oItemData = oData["ProductCollection"].filter(function(oCurItem) {
+					return oCurItem.ProductId === sProductId;
+				})[0]; 
+
+			if (oItemData) {
+				oView.setModel(new JSONModel(oItemData),
+					"detail");
+			}
+		},
 		getRouter: function() {
 			return sap.ui.core.UIComponent.getRouterFor(this);
 		},
-		handlePress: function() {
-			this.getRouter().navTo("detailDetailName", {
+		toggleFooter: function(oEvent) {
+			var oObjectPageLayout = this.getView().byId("ObjectPageLayout");
+			oObjectPageLayout.setShowFooter(!oObjectPageLayout.getShowFooter());
+		},
+		handlePress: function(oEvent) {
+			/*this.getRouter().navTo("detailDetailName", {
 				"detaildetail-item": "item - inner1 ",
 				"detail-item": "item1"
+			});*/
+			var sId = oEvent.getParameter("id"),
+				oSelectedItem = sap.ui.getCore().byId(sId),
+				// It's important to point that we're getting the second model, not the default one
+				oModel = oSelectedItem.getModel("detailDetail"),
+				sPath = oSelectedItem.getBindingContextPath(),
+				oData = oModel.getProperty(sPath);
+			this.getRouter().navTo("detailDetailName", {
+				"detaildetail-item": oData.ProductId,
+				"detail-item": this._itemDetail
 			});
+
 		}
 	});
 });
